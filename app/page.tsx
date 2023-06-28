575c4f48e848699e26f788d508664b8e31e9356c
Karan Nagpal <karan.nagpal@neosoftmail.com>
2023-06-28 14:02:33 +0530

    theme changes from tw-config file

--- app/page.tsx
+++ app/page.tsx
@@ -5,30 +5,35 @@ import { useEffect, useState } from "react";
 
 import { useTheme } from "next-themes";
 
+const themes = [
+  { name: "light" },
+  { name: "dark" },
+  { name: "green" },
+  { name: "brown" },
+];
 
 export default function Home() {
-
   const [mounted, setMounted] = useState(false);
 
-  const { resolvedTheme, setTheme } = useTheme();
+  const { theme, setTheme } = useTheme();
 
   useEffect(() => setMounted(true));
 
   return (
     <>
       <main className="px-2 container max-w-2xl mx-auto">
-        <h1>The main heading</h1>
-        <h2>level 2 heading</h2>
-        <p>
+        <h1 className="text-chalk-white">Main heading is in accent color</h1>
+        <h2 className="text-chalk-primary bg-charcoal-primary">level 2 heading, in primary color</h2>
+        <p className="text-chalk-secondary bg-charcoal-secondary">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto illum
           dolores molestias minus delectus, ratione nisi inventore esse quasi
           eum, sequi, odio rem hic blanditiis facere est velit expedita
           veritatis.
         </p>
-        <p>some text here</p>
+        <p className="text-skin-accent">some text here</p>
         <div className="border-2 border-white p-2 rounded m-2">
-          <h2>Another sub heading</h2>
-          <p>
+          <h2 className="text-charcoal-primary bg-chalk-primary">Another sub heading</h2>
+          <p className="text-charcoal-secondary bg-chalk-secondary">
             Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolor et
             dolores expedita, nihil suscipit ipsa adipisci officiis delectus
             numquam, voluptate eligendi ea provident est, obcaecati dicta earum
@@ -38,8 +43,21 @@ export default function Home() {
             voluptatibus, unde repellat sunt.
           </p>
         </div>
-        <p>Currently selecte theme: {mounted && resolvedTheme}</p>
-        <button onClick={() => {setTheme(resolvedTheme === "dark" ? "light" : "dark")}}>Change theme</button>
+        <p>Currently selecte theme: {mounted && theme}</p>
+        <select
+          name="theme"
+          id="theme-select"
+          className="bg-primary border-black border py-1 px-3"
+          onChange={(e) => setTheme(e.currentTarget.value)}
+          value={theme}
+        >
+          <option value="system">System</option>
+          {themes.map((t) => (
+            <option key={t.name.toLowerCase()} value={t.name.toLowerCase()}>
+              {t.name}
+            </option>
+          ))}
+        </select>
       </main>
     </>
   );
